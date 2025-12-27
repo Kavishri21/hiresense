08aabc1b5361206a80fd85f5f3d21d9462d55e78
Kavishri21 <dr.kavi21k@gmail.com>
2025-12-27 11:30:27 +0530

    Initial commit - HireSense backend

---
+++ hiresense_backend/hiresense_backend/src/main/java/com/hiresense/HiresenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hiresense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiresenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiresenseBackendApplication.class, args);
+	}
+
+}
---
+++ hiresense_backend/hiresense_backend/src/test/java/com/hiresense/HiresenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiresense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiresenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
